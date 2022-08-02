68e8f9dca5f5e92b7f10b4b0f90b3d0f08be2dff
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-08-02 17:18:11 -0300

    Includes new routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Habits from "../Habits/Habits";
+import History from "../History/History";
 import Login from "../Login/Login";
 import Register from "../Login/Register";
 import GlobalStyle from "../Shared/globalStyle";
+import Today from "../Today/Today";
 
 export default function App() {
     return (
@@ -11,6 +14,9 @@ export default function App() {
                 <Routes>
                     <Route path="/" element={<Login />} />
                     <Route path="/cadastro" element={<Register />} />
+                    <Route path="/habitos" element={<Habits />} />
+                    <Route path="/hoje" element={<Today />} />
+                    <Route path="/historico" element={<History />} />
                 </Routes>
             </BrowserRouter>
         </>
