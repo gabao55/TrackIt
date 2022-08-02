04a1de96e0d7dff1625fd7133c3ac42851c0d403
gabrielrosin <gabriel.s.rosin@gmail.com>
2022-08-02 09:24:37 -0300

    Setups project

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import GlobalStyle from "../Shared/globalStyle";
+
+export default function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <h1>Hello world</h1>
+        </>
+    )
+}
